dc187b7ce6b3efbe90fb18cf5ef4abeae9513d58
Gabriel Antonius <gabriel.antonius@gmail.com>
2013-11-04 13:36:58 -0500

    Refined the distribution.

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 
 from setuptools import find_packages
 
+install_requires = [
+    "matplotlib>=1.1",
+    "numpy>=1.6",
+    ]
+
 setup_args = dict(
       name             = 'pyatompaw',
-      version          = '1.3.0',
+      version          = '1.4.0',
       description      = "A python interface for atompaw.",
-     #long_description = long_description,
       author           = "Gabriel Antonius",
       author_email     = "gabriel.antonius@gmail.com",
      #url              = url,
      #license          = license,
      #platforms        = platforms,
      #keywords         = keywords,
-     #install_requires = install_requires,
+      install_requires = install_requires,
       packages         = find_packages(),
-     #scripts          = my_scripts
       )
 
 
